b2e13c3bfcff071b2ad5a87b4cd155d002dd08e0
NikolaLjepava <nikola.ljepava@gmail.com>
2022-11-14 14:43:15 +0100

    adding another automated test

---
+++ cypress/e2e/TestScenarios/testCase_Data_Vault.cy.js
@@ -0,0 +1,25 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false;
+});
+
+describe('Test case - Platform menu item', () => {
+
+	it('Open Data Vault', () => {
+
+		// Visit website
+		cy.visit('https://gamanza.com/')
+		// Navigate to Data Vault
+		cy.get('span').contains('Data Vault').click()
+
+		//Assertions
+		cy.url().should('include', '/digital-data-vault')
+		cy.contains('Digital Data Vault')
+		cy.contains('Features')
+		cy.contains('Regulation')
+		cy.contains('Encryption')
+		cy.contains('Reporting')
+		cy.contains('Integration')
+		cy.get('.vc_box_border_grey').find('img').should('have.attr','src').should('include','https://gamanza.com/wp-content/uploads/2021/03/datavault-locket-smallertinyjpg.png')
+		//cy.get('.vc_box_border_grey').find('img').should('have.attr','src').should('include','https://gamanza.com/wp-content/uploads/2021/03/gli-digital-no-border-smallertinyjpg.png')
+	})
+})
--- cypress/e2e/TestScenarios/testCase_Platform.cy.js
+++ cypress/e2e/TestScenarios/testCase_Platform.cy.js
@@ -34,6 +34,17 @@ describe('Test case - Platform menu item', () => {
 
 	})
 
-		it('Visit gamanza and open Platform', () => {
+		it('Open Data Vault', () => {
+
+			cy.get('span').contains('Data Vault').click()
+			cy.url().should('include', '/digital-data-vault')
+			cy.contains('Digital Data Vault')
+			cy.contains('Features')
+			cy.contains('Regulation')
+			cy.contains('Encryption')
+			cy.contains('Reporting')
+			cy.contains('Integration')
+			cy.get('.vc_single_image-img attachment-large').find('img').should('have.attr','src').should('include','https://gamanza.com/wp-content/uploads/2021/03/datavault-locket-smallertinyjpg.png')
+		//	cy.get('.vc_box_border_grey').find('img').should('have.attr','src').should('include','https://gamanza.com/wp-content/uploads/2021/03/gli-digital-no-border-smallertinyjpg.png')
 	})
 })
